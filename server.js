3a3ac4a9f1e2913af01b48bb41afe421e9bb8258
hantae <85547680+hantae96@users.noreply.github.com>
2023-06-11 15:33:21 +0900

    test

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(7070,()=>{
-    console.log('listening on 7070')
+app.listen(7070,function(){
+    console.log("listening on 7070")
+})
+
+app.get('/test',function(req,res){
+    res.send('test')
 })
