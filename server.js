51eee55ca1f41a097d7565a3acf5545e438d71b7
hantae <85547680+hantae96@users.noreply.github.com>
2023-06-11 15:45:14 +0900

    연동

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use(express.static(path.join(__dirname,'instar/build')))
+
 
 app.listen(7070,function(){
     console.log("listening on 7070")
@@ -7,4 +11,9 @@ app.listen(7070,function(){
 
 app.get('/test',function(req,res){
     res.send('test')
+})
+
+
+app.get('/',function(request,response){
+    response.sendFile(path.join(__dirname,'instar/build/index.html'))
 })
